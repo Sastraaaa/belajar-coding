420860f94cb23c070981de553d3589680624198d
Kharisma Sastrawansyah <kharismasastrawansyah@gmail.com>
2023-11-28 02:01:29 +0700

    Update kedua

---
+++ dasar.js
@@ -0,0 +1,45 @@
+let a = "a";
+const b = "b";
+var c = "c";
+
+let nama = "Rizky";
+let umur = 25;
+let apakahBenar = true;
+let uang = 5000.5;
+
+// operator arimatika
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 / angka2);
+console.log(angka1 * angka2);
+console.log(angka1 % angka2);
+
+// Belajar mengisi variabel
+let penampungHasil = angka1 + angka2;
+console.log(penampungHasil);
+penampungHasil = angka1 - angka2;
+console.log(penampungHasil);
+penampungHasil = angka1 / angka2;
+console.log(penampungHasil);
+penampungHasil = angka1 * angka2;
+console.log(penampungHasil);
+penampungHasil = angka1 % angka2;
+console.log(penampungHasil);
+
+// Penambahan String dengan operator aritmatika (tidak bisa dikurang)
+let firstName = "Bayu";
+let lastName = "Kokonut";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(firstName + " " + lastName);
+
+// alur pembacaan kode itu dari kiri ke kanan
+// contoh
+
+let number = 20;
+console.log(number + 10); //30
+number = 10;
+console.log(number + 10); //20
